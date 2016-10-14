1e121901f0da29fd748fba229d469341662e05ca
aexpositogonzalez <aexpositogonzalez@gmail.com>
2016-10-14 11:36:49 +0200

    Add files via upload

---
+++ examples.js
@@ -0,0 +1,17 @@
+
+
+jQuery("#pie").radialPieChart("init", {
+  'font-size': 13,
+  'fill': 20,
+  'data': [
+    {'color': "#2DB1E4", 'perc': 25},
+    {'color': "#9CCA13", 'perc': 50},
+    {'color': "#A4075E", 'perc': 15},
+    {'color': "#ff0000", 'perc': 10}
+  ]
+});
+
+
+
+
+
